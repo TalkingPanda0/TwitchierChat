152490de5289c12c4b4366e6390f8c4519984467
Talking Panda <memretwitch3626@gmail.com>
2026-06-21 22:27:19 +0300

    Fix replies being stored twice

--- src/main/java/dev/talkingpanda/twitchierchat/mixin/PlayerListMixin.java
+++ src/main/java/dev/talkingpanda/twitchierchat/mixin/PlayerListMixin.java
@@ -1,7 +1,11 @@
 package dev.talkingpanda.twitchierchat.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.talkingpanda.twitchierchat.Emotes;
+import dev.talkingpanda.twitchierchat.Replies;
 import net.minecraft.network.Connection;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.OutgoingChatMessage;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
 import net.minecraft.server.players.PlayerList;
 import net.minecraft.server.network.CommonListenerCookie;
@@ -9,12 +13,18 @@ import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.EnumSet;
 
 @Mixin(PlayerList.class)
 public abstract class PlayerListMixin {
+
+    @ModifyVariable(method = "broadcastChatMessage(Lnet/minecraft/network/chat/PlayerChatMessage;Ljava/util/function/Predicate;Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/network/chat/ChatType$Bound;)V", at = @At("STORE"), name = "tracked")
+    private static OutgoingChatMessage formatChatMessage(OutgoingChatMessage message, @Local(argsOnly = true, name = "chatType") ChatType.Bound chatType) {
+        return Replies.handleOutgoingMessage(message,chatType);
+    }
 
     @Inject(method = "placeNewPlayer",at = @At("TAIL"))
     private void sendEmotes(Connection connection, ServerPlayer player, CommonListenerCookie cookie, CallbackInfo ci) {
--- src/main/java/dev/talkingpanda/twitchierchat/mixin/ServerPlayerMixin.java
+++ src/main/java/dev/talkingpanda/twitchierchat/mixin/ServerPlayerMixin.java
@@ -1,18 +1,13 @@
 package dev.talkingpanda.twitchierchat.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.authlib.GameProfile;
-import dev.talkingpanda.twitchierchat.Replies;
-import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.OutgoingChatMessage;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayer.class)
@@ -22,10 +17,7 @@ public abstract  class ServerPlayerMixin extends Player {
         super(level, gameProfile);
     }
 
-    @ModifyVariable(method = "sendChatMessage", at = @At(value = "HEAD"), argsOnly = true, name = "message")
-    private static OutgoingChatMessage formatChatMessage(OutgoingChatMessage message, @Local(argsOnly = true, name = "chatType") ChatType.Bound chatType) {
-        return Replies.handleOutgoingMessage(message,chatType);
-    }
+
 
     @Inject(method = "getTabListDisplayName",at = @At("RETURN"),cancellable = true)
     private void colorTabListName(CallbackInfoReturnable<Component> cir) {
