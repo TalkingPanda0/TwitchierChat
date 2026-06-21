b5cfa28a1bd369fe5bda3eaeaf715446e8b66e30
Talking Panda <memretwitch3626@gmail.com>
2026-06-21 17:35:40 +0300

    Refactor Config

--- src/main/java/dev/talkingpanda/twitchierchat/Config.java
+++ src/main/java/dev/talkingpanda/twitchierchat/Config.java
@@ -15,109 +15,100 @@ import java.util.HashSet;
 import java.util.UUID;
 
 public class Config {
-    private static final HashSet<UUID> managers = new HashSet<>();
-    private static final HashMap<UUID, Integer> colorMap = new HashMap<>();
     private static final Path configPath = Path.of(TwitchierChat.configDir.toString(), "config.json");
     private static final File configFile = configPath.toFile();
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private static String serverAddress = null;
-    private static Integer serverPort = null;
+    private static final ConfigData configData = new ConfigData();
 
     public static String getServerUrl() {
-        if(serverAddress == null || serverPort == null ) {
+        if (configData.serverAddress == null || configData.serverPort == null) {
             return null;
         }
-        return serverAddress + ":" + serverPort;
-    }
-
-    public static String getServerAddress() {
-        return serverAddress;
-    }
-
-    public static void setServerAddress(String serverAddress) {
-        Config.serverAddress = serverAddress;
-        writeConfig();
-    }
-
-    public static Integer getServerPort() {
-        return serverPort;
+        return configData.serverAddress + ":" + configData.serverPort;
     }
 
     public static void setServer(String address, Integer serverPort) {
-        Config.serverAddress = address;
-        Config.serverPort = serverPort;
+        configData.serverAddress = address;
+        configData.serverPort = serverPort;
         writeConfig();
     }
 
     public static boolean addManager(UUID manager) {
-        boolean result = Config.managers.add(manager);
+        boolean result = configData.managers.add(manager);
         writeConfig();
 
         return result;
     }
 
     public static boolean removeManager(UUID manager) {
-        boolean result = Config.managers.remove(manager);
+        boolean result = configData.managers.remove(manager);
         writeConfig();
         return result;
     }
 
 
     public static boolean isManager(UUID player) {
-        return Config.managers.contains(player);
+        return configData.managers.contains(player);
     }
 
     public static void setColor(UUID player, Integer color) {
-        Config.colorMap.put(player, color);
+        configData.colorMap.put(player, color);
         writeConfig();
     }
 
     public static void removeColor(UUID player) {
-        Config.colorMap.remove(player);
+        configData.colorMap.remove(player);
         writeConfig();
     }
 
     public static @Nullable Integer getColor(UUID player) {
-        return Config.colorMap.get(player);
+        return configData.colorMap.get(player);
     }
 
 
     public static void readConfig() {
-        if (!configFile.exists()) {
-            Config.serverPort = TwitchierChat.minecraftServer.getServerPort();
-            writeConfig();
-            return;
-        }
+
         try (FileReader reader = new FileReader(configFile)) {
             ConfigData data = gson.fromJson(reader, ConfigData.class);
 
             if (data != null) {
-                Config.serverAddress = data.serverAddress;
+                if (data.serverAddress != null) {
 
-                Config.serverPort = data.serverPort;
-
-                Config.managers.clear();
-                if (data.managers != null) {
-                    Config.managers.addAll(data.managers);
+                    configData.serverAddress = data.serverAddress;
                 }
 
-                Config.colorMap.clear();
+                if(data.serverPort != null) {
+                    configData.serverPort = data.serverPort;
+                }
+
+                configData.managers.clear();
+                if (data.managers != null) {
+                    configData.managers.addAll(data.managers);
+                }
+
+                configData.colorMap.clear();
                 if (data.colorMap != null) {
-                    Config.colorMap.putAll(data.colorMap);
+                    configData.colorMap.putAll(data.colorMap);
                 }
             }
         } catch (IOException e) {
             TwitchierChat.LOGGER.error("Failed to read config file: ", e);
         }
+    }
 
+    public static Integer getMaxHistory() {
+        return configData.maxHistory;
+    }
 
+    public static void setMaxHistory(Integer maxHistory) {
+        configData.maxHistory = maxHistory;
+        writeConfig();
     }
 
     private static void writeConfig() {
-        ConfigData data = new ConfigData(Config.serverAddress, Config.serverPort, Config.managers, Config.colorMap);
 
         try (FileWriter writer = new FileWriter(configFile)) {
-            gson.toJson(data, writer);
+            gson.toJson(configData, writer);
         } catch (IOException e) {
             TwitchierChat.LOGGER.error("Failed to write config file: ", e);
 
@@ -125,16 +116,11 @@ public class Config {
     }
 
     private static class ConfigData {
-        private final String serverAddress;
-        private final Integer serverPort;
-        private final HashSet<UUID> managers;
-        private final HashMap<UUID, Integer> colorMap;
-
-        public ConfigData(String serverAddress, Integer serverPort, HashSet<UUID> managers, HashMap<UUID, Integer> colorMap) {
-            this.serverAddress = serverAddress;
-            this.serverPort = serverPort;
-            this.managers = managers;
-            this.colorMap = colorMap;
-        }
+        public String serverAddress = null;
+        public Integer serverPort = null;
+        public HashSet<UUID> managers = new HashSet<>();
+        public HashSet<UUID> dontPing = new HashSet<>();
+        public Integer maxHistory = 100;
+        public HashMap<UUID, Integer> colorMap = new HashMap<>();
     }
 }
